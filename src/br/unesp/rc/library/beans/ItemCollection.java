8af952cba424e34a4e652f7f9a56259e7e81fa4d
daltonbr <sacinopatinete@gmail.com>
2016-04-11 18:40:59 -0300

    beans package
    some basic elements

---
+++ src/br/unesp/rc/library/beans/ItemCollection.java
@@ -0,0 +1,17 @@
+package br.unesp.rc.library.beans;
+
+/**
+ * Created on 4/11/16 by:
+ Dalton Lima         github.com/daltonbr
+ Lucas Pinheiro      github.com/lucaspin
+
+ This project is under The MIT License (MIT)
+ github.com/daltonbr/LibraryManager
+
+ */
+public abstract class ItemCollection {
+    private int code;
+    private String name;
+    private String author;
+    private int year;
+}
---
+++ src/br/unesp/rc/library/beans/Media.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.library.beans;
+
+/**
+ * Created on 4/11/16 by:
+ Dalton Lima         github.com/daltonbr
+ Lucas Pinheiro      github.com/lucaspin
+
+ This project is under The MIT License (MIT)
+ github.com/daltonbr/LibraryManager
+
+ */
+public class Media extends ItemCollection{
+}
